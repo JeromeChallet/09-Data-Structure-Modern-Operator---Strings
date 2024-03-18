520c4d41f539ffabcdf3029363775aacf41ba3ef
AzureAD\JeromeChallet <cjerome@premiumartsinc.com>
2024-03-18 10:49:23 +0900

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -64,7 +60,23 @@ const restaurant = {
   },
 };
 ///////////////String Method Practice///////////////
-console.log(flights.split('+'));
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+//console log the string above separating at every + symbol
+//padstart will alignt text to the left
+for (const flight of flight.split('+')) {
+  //console.log(flights.split(';'));
+  //using destructuring to take out 4 pieces out of the array to display them
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
 /*
 ///////////////Working With String 3///////////////
 // split will split a string based on a divider 'string'
